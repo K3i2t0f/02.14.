fe64bf54f0b11352f1bea41e8a127361100ebe42
Kristóf Borbára <borbara.kristof@petrik.hu>
2025-02-14 11:38:35 +0100

    2.Feladat

--- 02.14/Program.cs
+++ 02.14/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Karakter> karakterek = [];
+
+            Beolvasas("karakterek.txt", karakterek);
+            LegmagasabbEletero(karakterek);
+        }
+
+        static void Beolvasas(string filnenev, List<Karakter> karakterek)
+        {
+            StreamReader sr = new(filnenev);
+
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                string sor = sr.ReadLine();
+                string[] szavak = sor.Split(';');
+
+                Karakter karakter = new(szavak[0], Convert.ToInt16(szavak[1]), Convert.ToInt16(szavak[2]), Convert.ToInt16(szavak[3]));
+                karakterek.Add(karakter);
+            }
+        }
+
+        static void LegmagasabbEletero(List<Karakter> karakterek)
+        {
+            int max = karakterek[0].Eletero;
+            int index = 0;
+            for (int i = 1; i < karakterek.Count; i++)
+            {
+                if (karakterek[i].Eletero > max)
+                {
+                    max = karakterek[i].Eletero;
+                    index = i;
+                }
+            }
+            Console.WriteLine($" 2.Feladat: {karakterek[index]}");
         }
     }
 }
